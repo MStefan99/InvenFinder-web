854f783ee812e8c5d71e6ee3614f2f702afda6cf
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2022-06-20 19:31:55 +0300

    converted permissions library

--- backend/lib/permissions.ts
+++ backend/lib/permissions.ts
@@ -1,37 +1,16 @@
-function makeEnum(obj) {
-	let i = 0;
-	const map = new Map();
-
-	for (const prop in obj) {
-		if (!map.has(prop)) {
-			map.set(i, prop);
-			obj[prop] = i;
-		} else {
-			delete obj[prop];
-		}
-
-		++i;
-	}
-
-	return Object.freeze(map);
+enum PERMISSIONS {
+	'EDIT_ITEM_AMOUNT',
+	'MANAGE_ITEMS',
+	'LOAN_ITEMS',
+	'MANAGE_USERS'
 }
-
-const PERMISSIONS = {
-	'EDIT_ITEM_AMOUNT': 0,
-	'MANAGE_ITEMS': 0,
-	'LOAN_ITEMS': 0,
-	'MANAGE_USERS': 0
-};
-
-// For later use
-const permissionEnum = makeEnum(PERMISSIONS);
 
 
 export default {
 	PERMISSIONS,
 
 
-	getPermissionValue(permissions) {
+	getPermissionValue(permissions: [PERMISSIONS]) {
 		let val = 0;
 
 		for (const p of permissions) {
