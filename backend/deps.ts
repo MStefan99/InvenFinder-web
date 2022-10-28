51b7ef64d26dfd7db293bc1d033953d427c218eb
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2022-10-28 13:08:56 +0300

    updated mysql dependency

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,4 +1,4 @@
-export { Client as DBClient } from 'https://deno.land/x/mysql@v2.10.2/mod.ts';
+export { Client as DBClient } from 'https://deno.land/x/mysql@v2.10.3/mod.ts';
 export { decode, encode } from 'https://deno.land/std@0.157.0/encoding/hex.ts';
 export {
 	Application,
